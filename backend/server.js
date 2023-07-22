9fb6098a0460a93f74ebe93ab81e3f0eedeb6f88
prakharpandeyyy <8.prakharpandey@gmail.com>
2023-07-22 11:48:26 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { readdirSync } = require("fs");
 const app = express();
-const options = {
-  origin: "http://localhost:3000/",
-  useSuccessStatus: 200,
-};
+app.use(express.json());
+app.use(cors());
 
-app.use(cors(options));
+/// routes
 
-app.get("/", (req, res) => {
-  res.send("welcome to clone");
-});
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+/// database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to database Successfully"))
+  .catch((err) => console.log("error in connected to database"));
 
 app.listen(8000, (req, res) => {
   console.log("Server is up on port 8000");
